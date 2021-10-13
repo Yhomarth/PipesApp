b1b1274bf5e9a7d9f0f4bdcce3fd702cb79cc6a8
Yhomarth Reyes <yhomarth@gmail.com>
2021-10-13 00:33:48 -0400

    Clase 141 completada, PrimeFlex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 
 // Módulo personalizado
 import { SharedModule } from './shared/shared.module';
+import { VentasModule } from './ventas/ventas.module';
 
 
 
@@ -18,7 +19,8 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRouterModule,
-    SharedModule
+    SharedModule,
+    VentasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/ventas/ventas.module.ts
+++ src/app/ventas/ventas.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PrimeNgModule } from '../prime-ng/prime-ng.module';
+
 import { BasicosComponent } from './pages/basicos/basicos.component';
 import { NoComunesComponent } from './pages/no-comunes/no-comunes.component';
 import { NumerosComponent } from './pages/numeros/numeros.component';
@@ -21,7 +23,8 @@ import { OrdenarComponent } from './pages/ordenar/ordenar.component';
     OrdenarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    PrimeNgModule
   ]
 })
 export class VentasModule { }
