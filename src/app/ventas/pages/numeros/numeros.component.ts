433aae49b6a3024ea255b4953c910258294b6223
Yhomarth Reyes <yhomarth@gmail.com>
2021-10-21 22:50:35 -0400

    Clase 146 completada, DecimalPipe

--- src/app/ventas/pages/numeros/numeros.component.ts
+++ src/app/ventas/pages/numeros/numeros.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-numeros',
@@ -6,11 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class NumerosComponent implements OnInit {
+export class NumerosComponent  {
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+  ventasNetas: number = 26509343.46280;
+  porcentaje: number = 0.47;
 
+
+
+ 
 }
