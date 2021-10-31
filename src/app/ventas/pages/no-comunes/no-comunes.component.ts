4b78b7bcc1b01ee578cba71ffe8219f70e64a221
Yhomarth Reyes <yhomarth@gmail.com>
2021-10-31 18:57:19 -0400

    Clase 150 completada, I18nPluralPipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -8,13 +8,21 @@ import { Component } from '@angular/core';
 })
 export class NoComunesComponent  {
 
+  // i18nselect
   nombre : string = 'Rosa';
   genero : string = 'femenino';
-
   mapingObj = {
     'masculino' : 'invitarlo',
     'femenino'  : 'invitarla'
   }
 
+  // i18nplural
+  clientes : string [] = ['Juan', 'Pedro', 'Maria'];
+
+  mapingObjClientes = {
+   '=0': 'no tenemos ningún cliente esperando.',
+   '=1' : 'un cliente esperando.',
+   'other' : '# clientes esperando.' 
+  }
 
 }
