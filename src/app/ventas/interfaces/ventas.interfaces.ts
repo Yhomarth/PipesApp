8eb753ee33bf7f71c85de0f1ac58c92bc8a9122c
Yhomarth Reyes <yhomarth@gmail.com>
2021-11-15 16:05:42 -0400

    Clase 163 completada, LLenar un PrimeTable con data

---
+++ src/app/ventas/interfaces/ventas.interfaces.ts
@@ -0,0 +1,15 @@
+
+
+export enum Color {
+  rojo, verde, azul, amarillo, blanco, negro, rosado, dorado
+}
+
+
+
+export interface Heroe {
+
+  nombre : string,
+  vuela : boolean,
+  color : Color
+
+}
--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
@@ -6,16 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class OrdenarComponent implements OnInit {
+export class OrdenarComponent  {
 
 
   enMayuscula : boolean = true;
+  heroes : Heroe[] = [
+    {
+      nombre : "Superman",
+      vuela : true,
+      color : Color.azul
+    },
+    {
+      nombre : "Batman",
+      vuela : false,
+      color : Color.negro
+    },
+    {
+      nombre : "Robin",
+      vuela : false,
+      color : Color.rojo
+    },
+    {
+      nombre : "Daredevil",
+      vuela : false,
+      color : Color.rojo
+    },
+    {
+      nombre : "Linterna verde",
+      vuela : true,
+      color : Color.verde
+    },
+  ]
 
   constructor() { }
-
-
-  ngOnInit(): void {
-  }
 
 
   btnMayusculas(){
